223b0ef3c93bf34e46aba666e8d623f27bc0497b
Igor Nikiforov <kolobamanacas@outlook.com>
2021-06-01 04:35:47 +0300

    New interfaces for files.

---
+++ src/interfaces/files/IFileInfo.ts
@@ -0,0 +1,5 @@
+export default interface FileInfo {
+  id: string;
+  categoryId: string;
+  originalName: string;
+}
---
+++ src/interfaces/files/IFileSet.ts
@@ -0,0 +1,4 @@
+export default interface IFileSet {
+  category: string;
+  files: File[];
+}
