d247c0ecd87fab5d566a3ddf61c12350f5cbd58e
Igor Nikiforov <kolobamanacas@outlook.com>
2021-06-01 05:18:17 +0300

    Merge with origin/develop.

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,17 +3,15 @@ import Vuex, { StoreOptions } from 'vuex';
 import { users } from '@/store/modules/users';
 import { RootState } from './types';
 
-import { auth } from './modules/auth';
-import { patients } from './modules/patients';
-import { representatives } from './modules/representatives';
-import documents from './modules/documents';
-import { insuranceCompanies } from './modules/insuranceCompanies';
-import { anthropometry } from './modules/anthropometry';
-import documentScans from './modules/documentScans';
-import { mkb } from './modules/mkb';
-import { disabilities } from './modules/disabilities';
 import sma from './modules/sma/sma';
+import { anthropometry } from './modules/anthropometry';
+import { auth } from './modules/auth';
+import { disabilities } from './modules/disabilities';
+import { insuranceCompanies } from './modules/insuranceCompanies';
+import { mkb } from './modules/mkb';
+import { patients } from './modules/patients';
 import { representativeTypes } from './modules/representativeTypes';
+import { representatives } from './modules/representatives';
 
 const store: StoreOptions<RootState> = {
   state: {
@@ -33,10 +31,8 @@ const store: StoreOptions<RootState> = {
     auth,
     patients,
     representatives,
-    documents,
     insuranceCompanies,
     anthropometry,
-    documentScans,
     mkb,
     disabilities,
     sma,
