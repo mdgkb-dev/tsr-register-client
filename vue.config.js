3a1cb24df72dc68666c398118cec94d79599c0a2
Igor Nikiforov <kolobamanacas@outlook.com>
2021-08-09 14:24:30 +0300

    Add port setting

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   lintOnSave: false,
   devServer: {
+    port: process.env.VUE_APP_PORT,
     disableHostCheck: true,
     host: 'localhost',
     proxy: process.env.DEV_BACKEND_URL,
