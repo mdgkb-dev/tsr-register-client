f9f11cdc62eae934650513ae27ecd0768dacc06d
Igor Nikiforov <kolobamanacas@outlook.com>
2021-08-18 07:41:47 +0300

    Make jest work with babel and mock static assets

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   transform: {
     '^.+\\.vue$': 'vue-jest',
   },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css|less|sass|scss)$': 'identity-obj-proxy',
+  },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,133 +1,22 @@
-import './router/componentHooks';
-import 'dayjs/locale/ru';
-import './assets/element-variables.scss';
-
-import {
-  ElAffix,
-  ElAside,
-  ElAutocomplete,
-  ElAvatar,
-  ElBreadcrumb,
-  ElBreadcrumbItem,
-  ElButton,
-  ElButtonGroup,
-  ElCard,
-  ElCheckbox,
-  ElCheckTag,
-  ElCol,
-  ElCollapse,
-  ElCollapseItem,
-  ElContainer,
-  ElDatePicker,
-  ElDescriptions,
-  ElDescriptionsItem,
-  ElDialog,
-  ElDivider,
-  ElDrawer,
-  ElFooter,
-  ElForm,
-  ElFormItem,
-  ElHeader,
-  ElIcon,
-  ElImage,
-  ElInfiniteScroll,
-  ElInput,
-  ElInputNumber,
-  ElLoading,
-  ElMain,
-  ElMenu,
-  ElMenuItem,
-  ElMenuItemGroup,
-  ElMessage,
-  ElMessageBox,
-  ElNotification,
-  ElOption,
-  ElPagination,
-  ElPopconfirm,
-  ElPopover,
-  ElRadio,
-  ElRow,
-  ElScrollbar,
-  ElSelect,
-  ElSpace,
-  ElSubmenu,
-  ElTable,
-  ElTableColumn,
-  ElTag,
-  ElTimeline,
-  ElTimelineItem,
-  ElTooltip,
-  ElTree,
-  ElUpload,
-} from 'element-plus';
 import { createApp } from 'vue';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
+import { components, plugins } from '@/plugins/ElementPlus';
 
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import 'dayjs/locale/ru';
+import '@/assets/element-variables.scss';
+import '@/router/componentHooks';
 
-// locale.use(lang);
-const components = [
-  ElAside,
-  ElAutocomplete,
-  ElAvatar,
-  ElAffix,
-  ElButton,
-  ElButtonGroup,
-  ElBreadcrumb,
-  ElBreadcrumbItem,
-  ElCard,
-  ElCheckbox,
-  ElCheckTag,
-  ElCol,
-  ElCollapse,
-  ElCollapseItem,
-  ElContainer,
-  ElDatePicker,
-  ElDialog,
-  ElDivider,
-  ElDrawer,
-  ElFooter,
-  ElForm,
-  ElFormItem,
-  ElHeader,
-  ElIcon,
-  ElImage,
-  ElInput,
-  ElInputNumber,
-  ElMain,
-  ElMenu,
-  ElMenuItem,
-  ElMenuItemGroup,
-  ElOption,
-  ElPopover,
-  ElPagination,
-  ElPopconfirm,
-  ElRow,
-  ElScrollbar,
-  ElSelect,
-  ElSubmenu,
-  ElSpace,
-  ElTable,
-  ElTableColumn,
-  ElTag,
-  ElTimeline,
-  ElTimelineItem,
-  ElTooltip,
-  ElTree,
-  ElUpload,
-  ElRadio,
-  ElDescriptions,
-  ElDescriptionsItem,
-];
-const plugins = [ElInfiniteScroll, ElLoading, ElMessage, ElMessageBox, ElNotification];
 const app = createApp(App);
 app.use(store);
 app.use(router);
 
-components.forEach((component) => {
+
+for (const [_, component] of Object.entries(components)) {
   app.component(component.name, component);
-});
+}
+
 plugins.forEach((plugin) => {
   app.use(plugin);
 });
