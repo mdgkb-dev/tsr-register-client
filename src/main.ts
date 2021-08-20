cb42cead7948a225bc1d06f87bcdd15014e8cca4
Ilia <sirtsevilya@yandex.ru>
2021-08-20 14:24:10 +0300

    Layout css hotfix

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
+// eslint-disable-next-line simple-import-sort/imports
 import { createApp } from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
 import { components, plugins } from '@/plugins/ElementPlus';
 
-import 'dayjs/locale/ru';
+import 'dayjs/locale/ru'; 
 import '@/assets/element-variables.scss';
 import '@/router/componentHooks';
 
