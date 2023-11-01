051e1f684abda1c86b621698100982f680816acd
Daniil <qwer.82@mail.ru>
2023-11-01 21:58:47 +0600

    Fix long label string

--- src/components/admin/Patients/AdminPatientPage.vue
+++ src/components/admin/Patients/AdminPatientPage.vue
@@ -8,7 +8,7 @@
       </div>
     </template>
     <template #body>
-      <InputNumber />
+      <!-- <InputNumber /> -->
       <component v-bind="menusProperties" :is="activeMenu.component" :key="componentKey" />
     </template>
   </MenuContainer>
