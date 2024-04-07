d5499a8d8a0558ef08d4bf9a4c85d2e5d48be2fb
Daniil <qwer.82@mail.ru>
2024-04-07 16:50:36 +0500

    Inport classes in vite

---
+++ src/services/ServicesNames.ts
@@ -0,0 +1,9 @@
+
+const ServicesNames = ['Contact', 'Email', 'Message', 'Provider', 'FTSP', 'Arrays', 'Axios', 'brandColors', 'buildNameNumbers',
+            'Cache', 'CarouselSwipe', 'ClassHelper', 'Color', 'Confirm', 'countRating', 'CreateSortModels', 
+            'Cursor', 'DateFormat', 'DateMask', 'Dates', 'DatesFormatter', 'Dragger', 'Favourite', 'GetExtension', 'HttpClient', 
+            'MakeCarousel', 'moves', 'Period', 'PhoneService',
+            'Router', 'Scheduler', 'Scroll', 'sort', 'Store', 'Strings', 'Swapper', 'Time', 'TimeFormatter', 'Token', 'useConfirmLeavePage',
+            'User', 'validate', 'WaitElement', 'WebSocketClient' ];
+
+export default ServicesNames;
